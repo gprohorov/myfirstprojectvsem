8086983777f11a54d28db95fc891a52d4d1ae123
gprohorov <yuramail222@gmail.com>
2020-05-26 10:31:11 +0300

    Initial commit

---
+++ src/pro/edu/Main.java
@@ -0,0 +1,31 @@
+package pro.edu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(" Hello, World!");
+
+        byte age = 22;
+        System.out.println("My age is " + age +" years");
+        short height = 185;
+        System.out.println("My height is " + height + " cm");
+        int anualSalary = 100000;
+        System.out.println(" My anual salary is about " + anualSalary);
+
+        long distanceCentavra = 10_000_000_000l;
+
+        System.out.println("From me to the nearest galaxy is about " + distanceCentavra);
+
+        //--------------------------------------------------------------
+
+        float weight = 85.5f;
+        double gardenArea = 60.0;
+        char myMark = '5';
+        boolean isStudent = true;
+        double b = 5.0 ;
+        char c = '%';
+
+
+
+    }
+}
